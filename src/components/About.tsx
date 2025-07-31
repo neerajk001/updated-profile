f8f9ae8d52bc9cd7aa80744e05e58ac54c50721d
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-08-01 00:58:48 +0530

    added skill icon

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { Badge } from './ui/badge'
+import Image from 'next/image'
 
 const experiences = [
   {
@@ -21,9 +22,20 @@ const experiences = [
 ]
 
 const skills = [
-  'JavaScript', 'TypeScript', 'React', 'Next.js', 'Node.js',
-  'MongoDB', 'Express.js', 'HTML', 'CSS', 'Tailwind CSS',
-  'Git', 'PostgreSQL', 'Python', 'REST APIs', 'Vercel', 'Netlify'
+  { name: 'JavaScript', icon: '/js.png' },
+  { name: 'TypeScript', icon: '/typescript.png' },
+  { name: 'React', icon: '/react.png' },
+  { name: 'Next.js', icon: '/next.png' },
+  { name: 'Node.js', icon: '/node.png' },
+  { name: 'MongoDB', icon: '/mongo.png' },
+  { name: 'Express.js', icon: '/express.png' },
+  { name: 'Tailwind CSS', icon: '/tailwind.png' },
+  { name: 'Git', icon: '/git.png' },
+  { name: 'PostgreSQL', icon: '/sql.png' },
+  { name: 'Vercel', icon: '/newVercel.png' },
+  { name: 'Netlify', icon: '/netlify.png' },
+  { name: 'AWS', icon: '/aws.png' },
+
 ]
 
 const About = () => {
@@ -57,11 +69,14 @@ const About = () => {
           <div className="flex flex-wrap gap-4">
             {skills.map((skill) => (
               <Badge
-                key={skill}
+                key={skill.name}
                 variant="secondary"
                 className="text-white text-base border bg-black border-white px-3 py-1"
               >
-                {skill}
+                <Image width={20}
+                height={20}
+                src={skill.icon} alt={skill.name} className="w-5 h-5 space-x-2" />
+      {skill.name}
               </Badge>
             ))}
           </div>
