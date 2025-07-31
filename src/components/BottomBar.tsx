67a46d07dea293dfb3850661d988c65d500be8d2
neeraj kushwaha <workwithneeraj.01@gmail.com>
2025-08-01 01:34:43 +0530

    added padding

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -29,14 +29,14 @@ const socialLinks = [
 
 export default function BottomBar() {
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-black px-4 py-2 rounded-xl flex gap-4 border border-white z-50">
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 bg-black px-6 py-1.5 rounded-xl flex gap-4 border border-white z-50">
       {socialLinks.map((icon, index) => (
         <Link key={index} href={icon.href} target="_blank" rel="noopener noreferrer">
           <Image
             src={icon.src}
             alt={icon.alt}
-            width={30}
-            height={30}
+            width={35}
+            height={35}
             className="invert border-2 border-white rounded-md p-1 hover:scale-110 transition"
           />
         </Link>
